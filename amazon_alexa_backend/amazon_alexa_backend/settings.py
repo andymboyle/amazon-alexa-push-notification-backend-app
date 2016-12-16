dbe304edd10f948e2e51b97eca7246a401d57132
Andy Boyle <andy.boyle@nbcuni.com>
2016-12-16 15:42:13 -0600

    adding a local_settings.py file

--- amazon_alexa_backend/amazon_alexa_backend/settings.py
+++ amazon_alexa_backend/amazon_alexa_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n=gmxwn(!i=%nvxw_jwi+r+lo!t1$(our+4hifzrsr1e(s6pyn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
